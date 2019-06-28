198b0437780d7a4c63c97c1303f9217eeb5ea1b7
anees05 <anees.adams05@gmail.com>
2019-06-28 17:39:05 -0400

    completed html and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var disneyMovies = ["lion king", "toy story", "little mermaid", "tarzan", "aladdin", "hercules", "the jungle book", "mulan"];
+var disneyMovies = ["lionking", "toystory", "littlemermaid", "tarzan", "aladdin", "hercules", "thejunglebook", "mulan"];
 
 var wins = [];
 var losses = [];
@@ -9,14 +9,14 @@ var lettersInWord = [];
 var word = "";
 var wrongGuesses = [];
 var blanksAndCorrectGuesses = [];
+var letterguessed;
 
 
 
 // Reset
-function start(){
-    document.getElementById("remaining-guesses").innerHTML = amountOfGuesses;
-    document.getElementById("amount-blank").innerHTML = blanksAndCorrectGuesses.join(" ");
-    document.getElementById("incorrect-guesses").innerHTML = wrongGuesses.join(" ");
+function start() {
+   
+    word = disneyMovies[Math.floor(Math.random() * disneyMovies.length)];
 
     amountOfGuesses = 6;
     blanksAndCorrectGuesses = [];
@@ -25,10 +25,72 @@ function start(){
     lettersInWord = word.split("");
     console.log(word);
 
-    for (var c = 0; c < blanks; c++){
+    for (var c = 0; c < word.length; c++) {
         blanksAndCorrectGuesses.push("_");
-    }console.log(blanksAndCorrectGuesses);
+    } console.log(blanksAndCorrectGuesses);
 
-    word = disneyMovies[Math.floor(Math.random() * disneyMovies.length)];
+    document.getElementById("remaining-guesses").innerHTML = amountOfGuesses;
+    document.getElementById("blank-amount").innerHTML = blanksAndCorrectGuesses.join(" ");
+    document.getElementById("incorrect-guesses").innerHTML = wrongGuesses.join(" ");
+}
+
+// Word Check
+function checkGuess(guess) {
+
+    var inWord = false;
+
+    if (word.includes(letterguessed)) {
+
+        for (var i = 0; i < word.length; i++) {
+            if (word[i] === letterguessed) {
+                blanksAndCorrectGuesses[i] = letterguessed;
+            }
+        }
+
+    } else {
+
+        if (wrongGuesses.includes(letterguessed)) {
+            alert("pick a letter you have not guessed");
+        } else {
+            amountOfGuesses--;
+            wrongGuesses.push(guess);
+        }
+
+    };
+
+}
+// Restart Round
+function roundOver() {
+    console.log("Win:" + wins + "Loss" + losses + "AmountofGuesses" + amountOfGuesses);
+
+    document.getElementById("remaining-guesses").innerHTML = amountOfGuesses;
+    document.getElementById("blank-amount").innerHTML = blanksAndCorrectGuesses.join(" ");
+    document.getElementById("incorrect-guesses").innerHTML = wrongGuesses.join(" ");
+
+    if (lettersInWord.toString() === blanksAndCorrectGuesses.toString()) {
+        wins++;
+        alert("You Win!");
+
+        document.getElementById("wins").innerHTML = wins;
+        start();
+    }
+
+    else if (amountOfGuesses === 0) {
+        losses++
+        alert("You Lose");
+        document.getElementById("losses").innerHTML = losses;
+        start();
+    }
+}
+
+// Running App
+start();
+
+document.onkeyup = function (event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        letterguessed = event.key.toLocaleLowerCase();
+        checkGuess(letterguessed);
+        roundOver();
+    }
 }
 
